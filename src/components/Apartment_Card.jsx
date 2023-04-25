2d05b2d640fddc48239a7cb380a9629e039f930e
Deflandre Melvyn <deflandremelvyn@hotmail.fr>
2023-04-25 20:36:51 +0200

    Mep css homepage + about, prochaine étape modif description panel

--- src/components/Apartment_Card.jsx
+++ src/components/Apartment_Card.jsx
@@ -3,14 +3,12 @@ import "../styles/Apartment_Card.scss";
 import { Link } from "react-router-dom";
 
 function Apartment(props) {
-  console.log("hello props dans apartment card:", props);
   return (
     <div className="apartment">
-      <img src={props.cover} alt="" />
-
-      <div className="apartment_title">
-        <Link to="/logement">{props.title}</Link>
-      </div>
+      <Link to="/logement">
+        <img src={props.cover} alt="" />
+        <div className="apartment_title">{props.title}</div>
+      </Link>
     </div>
   );
 }
--- src/components/Container_apartments.jsx
+++ src/components/Container_apartments.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Apartment_Card from "./Apartment_Card.jsx";
 import "../styles/Container_apartments.scss";
-
+import logements from "../datas/logements.json";
 function Container_apartments() {
-  const [logements, setLogements] = useState([]);
-  useEffect(CallApi, []);
-  function CallApi() {
-    fetch("logements.json")
-      .then((res) => res.json())
-      .then((res) => setLogements(res))
-      .catch(console.error);
-  }
+  // const [logements, setLogements] = useState([]);
+  // useEffect(CallApi, []);
+  // function CallApi() {
+  //   const resultat = ("../datas/logements.json");
+  //   setLogements(resultat);
+  // .then((res) => console.log(res))
+  // .catch(console.error);
+  // }
+
+  console.log(logements);
   return (
     <div className="container">
       {logements.map((logement) => (
