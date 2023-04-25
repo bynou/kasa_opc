ac00194a25293fb40a8b8f8b238782e5008b67a6
Deflandre Melvyn <deflandremelvyn@hotmail.fr>
2023-04-25 23:20:06 +0200

    rename du composant description_panel en Collapse

--- src/components/Description_Panel.jsx
+++ src/components/Description_Panel.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
-import "../styles/Description_Panel.scss";
+import "../styles/Collapse.scss";
 
-function Description_Panel(props) {
+function Collapse(props) {
   const [contentVisibleOff, setContentVisibleOn] = useState(false);
   const displayContent = () => {
     setContentVisibleOn(!contentVisibleOff);
   };
   const contentClass =
     (contentVisibleOff ? "visible" : "hidden") + "description_content";
+  //Si visible on modifie le chevron en passant par sa classe
   const chevronClass =
     (contentVisibleOff ? "fa-chevron-up" : "fa-chevron-down") + " fas";
   return (
-    <div className="Description_Panel">
-      <p className="Description_Panel_header" onClick={displayContent}>
+    <div className="Collapse">
+      <p className="Collapse_header" onClick={displayContent}>
         <span>{props.title}</span>
         <i className={chevronClass}></i>
       </p>
+      {/*Si visible alors affiche contenu de p */}
       {contentVisibleOff && <p className={contentClass}>{props.content}</p>}
     </div>
   );
 }
 
-export default Description_Panel;
+export default Collapse;
